8566d4c4d735258d442601af25194016324aff96
KlafaKEN <klllafaken@gmail.com>
2025-07-28 01:02:44 +0300

    prepreend

--- main.js
+++ main.js
@@ -39,3 +39,38 @@ document.addEventListener('DOMContentLoaded', () => {
     console.warn('burger or header-modal not found');
   }
 });
+
+
+const update = () => {
+  const AllArticles = document.querySelectorAll('.articles-card').length;
+  document.querySelectorAll('.AllArticles').forEach(span => {
+    span.textContent = AllArticles;
+  });
+    const GameGuides = document.querySelectorAll('.game-guides-article').length;
+  document.querySelectorAll('.GameGuides').forEach(span => {
+    span.textContent = GameGuides;
+  });
+    const CasinoReviews = document.querySelectorAll('.casino-reviews').length;
+  document.querySelectorAll('.CasinoReviews').forEach(span => {
+    span.textContent = CasinoReviews;
+  });
+      const Bonuses = document.querySelectorAll('.bonuses-article').length;
+  document.querySelectorAll('.Bonuses').forEach(span => {
+    span.textContent = Bonuses;
+  });
+      const Technology = document.querySelectorAll('.technology-article').length;
+  document.querySelectorAll('.Technology').forEach(span => {
+    span.textContent = Technology;
+  });
+      const Safety = document.querySelectorAll('.safety-article').length;
+  document.querySelectorAll('.Safety').forEach(span => {
+    span.textContent = Safety;
+  });
+};
+
+update(); // первый подсчет
+
+// следим только за контейнером с карточками
+const container = document.querySelector('.articles-container');
+const mo = new MutationObserver(update);
+mo.observe(container, { childList: true, subtree: true });
